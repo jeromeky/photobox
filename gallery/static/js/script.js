1cb9320c499a3e8a1c694b2094d3a4af21ff5354
jeromeky@gmail.com <jeromeky@gmail.com>
2013-04-08 00:09:03 +0200

    change prettyphoto to swipebox

--- gallery/static/js/script.js
+++ gallery/static/js/script.js
@@ -1,8 +1,12 @@
 
 function loadPrettyPhoto(){
+/*
 	$(".portfolio a[rel^='prettyPhoto']").prettyPhoto({
 		theme:'light_square',
 		autoplay_slideshow: false,
 		overlay_gallery: false
 	});
+*/
+$(".swipebox").swipebox();
 }
+
